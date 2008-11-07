ac8c9ebe4914293fbff8648a6d22b8d6a5dbff0e
Shawn Chin <shawnchin@gmail.com>
2008-11-07 10:50:35 +0000

    fixed example for MB_Iterator_GetMessage()

--- example/snippets/ex_mb_it_getmessage.c
+++ example/snippets/ex_mb_it_getmessage.c
@@ -10,14 +10,14 @@ void func_ayam(void) {
     /* create and iterator myBoard */
     MB_Iterator_Create(myBoard, &iterator);
     
-    rc = MB_Iterator_GetMessage(iterator, (void *)msg);
+    rc = MB_Iterator_GetMessage(iterator, (void **)&msg);
     while (msg) /* loop till end of Iterator */
     {
         do_something_with_message(msg);
         free(msg); /* free allocated message */
         
         /* get next message from iterator */
-        rc = MB_Iterator_GetMessage(iterator, (void *)msg);
+        rc = MB_Iterator_GetMessage(iterator, (void **)&msg);
         
         if (rc != MB_SUCCESS)
         {
